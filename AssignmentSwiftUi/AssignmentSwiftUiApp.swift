f70faa5938db3e329e5478ee267e5de1fff157f0
karma <karmatashi954@gmail.com>
2022-03-15 18:54:50 +0545

    Initial Commit

---
+++ AssignmentSwiftUi/AssignmentSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentSwiftUiApp.swift
+//  AssignmentSwiftUi
+//
+//  Created by karma on 3/15/22.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentSwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AssignmentSwiftUi
+//
+//  Created by karma on 3/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
